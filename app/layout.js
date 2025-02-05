04b0857bafbb984e2f6a841c9ddf4533683bdb98
Kai Pereira <88850028+KaiPereira@users.noreply.github.com>
2025-02-05 20:24:19 +0000

    quick save

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,15 @@
 import { Silkscreen } from "next/font/google";
 import "./globals.css";
+import { Press_Start_2P } from "next/font/google";
+import "nes.css/css/nes.min.css";
 
 const silkscreen = Silkscreen({
   weight: '400',
   variable: "--font-silkscreen",
   subsets: ["latin"],
 });
+
+const pressStart2P = Press_Start_2P({ weight: "400", subsets: ["latin"] });
 
 export const metadata = {
   title: "Scrapyard Victoria - Build stupid s#!t, get stupid prizes",
@@ -14,10 +18,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${silkscreen.variable}`}>
-        {children}
-      </body>
-    </html>
+	<html lang="en">
+		<body className={`${silkscreen.variable} ${pressStart2P.variable}`}>
+		{children}
+		</body>
+	</html>
   );
 }
--- app/page.js
+++ app/page.js
@@ -3,7 +3,21 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-
+      {/* <button type="button" className="nes-btn is-success">Success</button> */}
+      <div className={styles.NES}>
+        <div className={styles.alignement}>
+          <h1 className={styles.header}>Scrapyard</h1>
+          <h2 className={styles.subHeader}>Victoria, BC</h2>
+          <div className={styles.mainText}>
+            <p>
+              Coming March 15th, Scrapyard victoria will be an in-person hackathon where participants will be challenged to build stupid s#!t and get stupid prizes. 
+            </p>
+            <p>
+              from 8am to 11pm, Bring your laptop and we’ll provide food, water and give out prizes at the end!
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
