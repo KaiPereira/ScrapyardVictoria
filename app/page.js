4d888cd26419ac4744737d7346166773f72077af
Kai Pereira <88850028+KaiPereira@users.noreply.github.com>
2025-02-06 22:13:17 +0000

    ghosts

--- app/page.js
+++ app/page.js
@@ -13,12 +13,19 @@ export default function Home() {
               Coming March 15th, Scrapyard victoria will be an in-person hackathon where participants will be challenged to build stupid s#!t and get stupid prizes. 
             </p>
             <p>
-              From 8am to 11pm, Bring your laptop to "777 Fort Street" and we’ll provide food, water and give out prizes at the end!
+              From 9am to 10pm, Bring your laptop to "777 Fort Street" and we’ll provide food, water and give out prizes at the end!
             </p>
           </div>
           <button type="button" className={`nes-btn is-success ${styles.button}`}>Signup</button>
         </div>
-        <img src="/ghost_red.svg" alt="blue ghost" className={styles.ghost} />
+        <img src="/ghost_red.svg" alt="blue ghost" className={styles.ghost_red1} />
+        <img src="/ghost_red.svg" alt="blue ghost" className={styles.ghost_red2} />
+
+        <img src="/ghost_orange.svg" alt="blue ghost" className={styles.ghost_orange1} />
+        <img src="/ghost_orange.svg" alt="blue ghost" className={styles.ghost_orange2} />
+
+        <img src="/ghost_blue.svg" alt="blue ghost" className={styles.ghost_blue1} />
+        <img src="/ghost_blue.svg" alt="blue ghost" className={styles.ghost_blue2} />
       </div>
     </>
   );
