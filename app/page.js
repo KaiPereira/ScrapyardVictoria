fe061abb0fac51843b38c129a5bdabd511026870
Kai Pereira <88850028+KaiPereira@users.noreply.github.com>
2025-02-06 22:27:54 +0000

    fixed some wording

--- app/page.js
+++ app/page.js
@@ -3,17 +3,16 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <>
-      {/* <button type="button" className="nes-btn is-success">Success</button> */}
       <div className={styles.NES}>
         <div className={styles.alignement}>
           <h1 className={styles.header}>Scrapyard</h1>
           <h2 className={styles.subHeader}>Victoria, BC</h2>
           <div className={styles.mainText}>
             <p>
-              Coming March 15th, Scrapyard victoria will be an in-person hackathon where participants will be challenged to build stupid s#!t and get stupid prizes. 
+              Coming March 15th, Scrapyard Victoria will be an in-person, highschool hackathon where participants will be challenged to build stupid s#!t and get stupid prizes.  
             </p>
             <p>
-              From 9am to 10pm, Bring your laptop to "777 Fort Street" and we’ll provide food, water and give out prizes at the end!
+              The event will take place for highschoolers at "777 Fort Street" from 9am to 10pm. Bring your laptop (and hardware if you want) and we'll provide everyone with food, drinks and everything else! 
             </p>
           </div>
           <button type="button" className={`nes-btn is-success ${styles.button}`}>Signup</button>
